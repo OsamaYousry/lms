56ce2a6fd3eaeffd5efb75298556e669d7e061ed
Osama ElKady <osama.yousry@outlook.com>
2024-05-23 17:59:14 +0200

    add navigationˆ

--- apps/lms-fe/src/app/components/toolbar/toolbar.tsx
+++ apps/lms-fe/src/app/components/toolbar/toolbar.tsx
@@ -1,12 +1,20 @@
-import {AppBar, Toolbar, Typography} from "@mui/material";
+import {AppBar, Box, Toolbar, Typography} from "@mui/material";
 
 export const LMSToolbar: React.FC = () => {
   return (
     <AppBar component="nav" position="sticky">
       <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        <Typography variant="h6" component="div">
           LMS
         </Typography>
+        <Box display="flex" flexDirection="row" gap="1rem" marginLeft="3rem">
+          <Typography variant="body1">
+            <a href="/courses">Courses</a>
+          </Typography>
+          <Typography variant="body1">
+            <a href="/students">Students</a>
+          </Typography>
+        </Box>
       </Toolbar>
     </AppBar>
   )
