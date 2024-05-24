9645dc1f0082e22f5be7582987cf45131cf320b3
Osama ElKady <osama.yousry@outlook.com>
2024-05-24 11:37:22 +0200

    insert query client inside the app

--- apps/lms-fe/src/app/app.tsx
+++ apps/lms-fe/src/app/app.tsx
@@ -4,19 +4,17 @@ import {MainRouter} from "./main-router";
 import {Box} from "@mui/material";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
-
 export function App() {
+
+  const queryClient = new QueryClient();
   const router = MainRouter();
   return (
-    <>
-      <QueryClientProvider client={queryClient}>
-        <LMSToolbar/>
-        <Box component="main" sx={{p: 3}}>
-          <RouterProvider router={router}/>
-        </Box>
-      </QueryClientProvider>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <LMSToolbar/>
+      <Box component="main" sx={{p: 3}}>
+        <RouterProvider router={router}/>
+      </Box>
+    </QueryClientProvider>
   );
 }
 
